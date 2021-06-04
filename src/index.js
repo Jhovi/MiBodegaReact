c031f46757464e59aa3a60fed99f9dfa1aa99cfa
Pablo Galarza <pgalarza@itana.pe>
2021-06-04 16:14:25 -0500

    Modificacion de url de backend

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Axios from 'axios';
 
-Axios.defaults.baseURL='http://localhost:5000/api/'
+Axios.defaults.baseURL='https://mibodegaapi.azurewebsites.net/api/'
 
 Axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token')
 
